f8d85a3ca647fe27a52edbc3c408779313e1c9f7
juanmanoel237 <ndeguemanoeljuan@gmail.com>
2024-11-13 10:35:33 +0100

    Ajustement du collapse version mobile

--- kasa/src/Components/Collapse/index.jsx
+++ kasa/src/Components/Collapse/index.jsx
@@ -1,58 +1,57 @@
+// Collapse.jsx
 import { useState, useRef, useEffect } from "react";
 import PropTypes from "prop-types";
 import "./collapse.scss";
 import arrow from '../../assets/Image/arrow/down.png';
 
-
 const Collapse = ({ collapseTitle, collapseDescription }) => {
   const [isOpen, setIsOpen] = useState(false);
-  
   const contentRef = useRef(null);
+  const innerContentRef = useRef(null);
 
-  // Fonction pour inverser l'état d'ouverture du collapse
   const toggleCollapse = () => {
     setIsOpen(prevState => !prevState);
   };
 
   useEffect(() => {
     const content = contentRef.current;
+    const innerContent = innerContentRef.current;
+    
     if (isOpen) {
-      // Si le collapse est ouvert, on définit la hauteur maximale comme étant la hauteur du contenu
-      content.style.maxHeight = `${content.scrollHeight}px`;
+      const height = innerContent.getBoundingClientRect().height;
+      content.style.maxHeight = `${height}px`;
     } else {
-      content.style.maxHeight = "0px";
+      content.style.maxHeight = '0px';
     }
-  }, [isOpen]); //useEffect sera déclenché à chaque fois que isOpen change
+  }, [isOpen]);
 
   return (
     <div className="collapse-container">
-      {/* Conteneur du titre qui, lorsqu'il est cliqué, déclenche le basculement de l'état */}
       <div className="collapse-title-container" onClick={toggleCollapse}>
         <div className="collapse-title">
           {collapseTitle}
-          {/* Image de la flèche qui change d'orientation en fonction de l'état isOpen */}
           <img
             src={arrow}
             alt="flêche"
-            className={`arrow-icon ${isOpen ? "arrow-up" : "arrow-down"}`}
+            className={`arrow-icon ${isOpen ? "arrow-up" : ""}`}
           />
         </div>
       </div>
-      {/* Conteneur de la description du collapse, avec une référence pour ajuster la hauteur */}
       <div
         ref={contentRef}
-        className={`collapse-description ${isOpen ? "open" : ""}`}
+        className={`collapse-description-wrapper ${isOpen ? "open" : ""}`}
       >
-        {collapseDescription}
+        <div ref={innerContentRef} className="collapse-description">
+          {collapseDescription}
+        </div>
       </div>
     </div>
   );
 };
 
-// Définition des prop types pour assurer que les bonnes propriétés sont passées au composant
 Collapse.propTypes = {
   collapseTitle: PropTypes.string.isRequired,
-  collapseDescription: PropTypes.node.isRequired, // Description du collapse, peut être n'importe quel noeud React
+  collapseDescription: PropTypes.node.isRequired,
 };
 
 export default Collapse;
